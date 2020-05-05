8d16495926f889c0429f5ea3bd72ea0279e33606
Aleksey Koltakov <aleksey.koltakov@gmail.com>
2020-05-06 00:13:49 +0300

    Implemented the subtitle switching feature.

--- src/tip.c
+++ src/tip.c
@@ -58,14 +58,19 @@ static int  InputEvent      ( vlc_object_t *, char const *,
                               vlc_value_t, vlc_value_t, void * );
 static void TimerEvent      ( void * );
 
-static void ChangeAudioTrack( input_thread_t *, int64_t );
 static void ChangeInput     ( intf_thread_t *, input_thread_t * );
 static void ChangeVout      ( intf_thread_t *, vout_thread_t * );
+static void SetVarByValueIndex( input_thread_t *, char const *, int64_t );
 
-const char* MAIN_AUDIOTRACK_NUM = "tip-main_audiotrack";
-const char* TRANSLATED_AUDIOTRACK_NUM = "tip-translated_audiotrack";
+const char* TRANSLATE_AUDIOTRACK_NUM = "tip-translate_audiotrack";
+const char* TRANSLATE_SUBTRACK_NUM = "tip-translate-subtrack";
+const char* REPEAT_SUBTRACK_NUM = "tip-repeat-subtrack";
 const char* TIME_TO_TRANSLATE = "tip-time-to-translate";
 
+#define ChangeSubTrack( p_input, ... ) \
+    SetVarByValueIndex( p_input, "spu-es",  __VA_ARGS__ );
+#define ChangeAudioTrack( p_input, ... ) \
+    SetVarByValueIndex( p_input, "audio-es",  __VA_ARGS__ );
 #define DisplayMessage( vout, ... ) \
     vout_OSDText( vout, VOUT_SPU_CHANNEL_OSD, SUBPICTURE_ALIGN_TOP | SUBPICTURE_ALIGN_LEFT,  __VA_ARGS__ );
 
@@ -76,12 +81,20 @@ vlc_module_begin ()
     set_text_domain( DOMAIN )
     set_shortname( N_("TIP") )
     set_description( N_("TIP (translate it, please)") )
+    set_help( N_("<sup><b>v1.1.0</b></sup><br/>The plugin adds new keyboard shortcuts to VLC:<ul><li>&nbsp;&lt;<b>/</b>&gt; for translation the specified period of video</li><li>&nbsp;&lt;<b>Shift</b> + <b>/</b>&gt; for repeat the last translated part of video</li></ul>") )
     set_capability( "interface", 0 )
     set_category( CAT_INTERFACE )
     set_subcategory( SUBCAT_INTERFACE_CONTROL )
-    add_integer( MAIN_AUDIOTRACK_NUM, 1, N_("Main audio track"), N_("The main audio track number"), false )
-    add_integer( TRANSLATED_AUDIOTRACK_NUM, 2, N_("Auxiliary audio track (for translation)"), N_("The auxiliary audio track number (for translation)"), false )
-    add_integer( TIME_TO_TRANSLATE, 5, N_("Period of time for translation (in seconds)"), N_("The period of time for play again with a translated audio track (in seconds)"), false )
+
+    add_integer( TIME_TO_TRANSLATE, 5, N_("Period of time for translation/repeat (in seconds)"), N_("The period of time for play again with a translated audio track (in seconds)"), false )
+
+    set_section( N_("Translation settings"), NULL )
+    add_integer( TRANSLATE_AUDIOTRACK_NUM, 1, N_("Audio track"), N_("The audio track number for translation (set -1 to disable audio track switch)"), false )
+    add_integer( TRANSLATE_SUBTRACK_NUM, 1, N_("Subtitle track"), N_("The subtitle track number for translation (set -1 to disable subtitle switch)"), false )
+
+    set_section( N_("Repeat settings"), NULL )
+    add_integer( REPEAT_SUBTRACK_NUM, 2, N_("Subtitle track"), N_("The subtitle track number for repeat (set -1 to disable subtitle switch)"), false )
+
     set_callbacks( Open, Close )
 vlc_module_end ()
  
@@ -92,6 +105,10 @@ struct intf_sys_t
 {
     vlc_mutex_t      lock;
     vlc_timer_t      timer;
+    
+    vlc_value_t origin_audiotrack;
+    vlc_value_t origin_subtrack;
+    bool is_origin;
     
     int64_t          i_start_time;
     int64_t          i_end_time;
@@ -113,6 +130,9 @@ static int Open( vlc_object_t *p_this )
         return VLC_ENOMEM;
     
     p_intf->p_sys = p_sys;
+    p_sys->origin_audiotrack.i_int = -1;
+    p_sys->origin_subtrack.i_int = -1;
+    p_sys->is_origin = true;
     p_sys->i_start_time = -1;
     p_sys->i_end_time = -1;
     p_sys->p_vout = NULL;
@@ -159,59 +179,68 @@ static void Close( vlc_object_t *p_this )
 static int KeyboardEvent( vlc_object_t *libvlc, char const *psz_var,
                           vlc_value_t oldval, vlc_value_t newval, void *p_data )
 {
+    msg_Dbg( libvlc, "key pressed: 0x%" PRIx64, newval.i_int );
+    if ( newval.i_int != KEY_SLASH && newval.i_int != KEY_SHIFT_QUESTION )
+        return VLC_SUCCESS;
+
     VLC_UNUSED( psz_var );
     VLC_UNUSED( oldval );
     intf_thread_t *p_intf = (intf_thread_t *)p_data;
     intf_sys_t *p_sys = p_intf->p_sys;
     
-    msg_Dbg( libvlc, "key pressed: 0x%" PRIx64, newval.i_int );
-    
-    vlc_mutex_lock( &p_intf->p_sys->lock );
+    vlc_mutex_lock( &p_sys->lock );
     input_thread_t *p_input = p_sys->p_input ? vlc_object_hold( p_sys->p_input )
                                              : NULL;
     vout_thread_t *p_vout = p_sys->p_vout ?    vlc_object_hold( p_sys->p_vout )
                                              : NULL;
-    vlc_mutex_unlock( &p_intf->p_sys->lock );
-    
-    switch ( newval.i_int )
+    vlc_mutex_unlock( &p_sys->lock );
+
+    if ( p_input && p_vout && var_GetBool( p_input, "can-seek" ) )
     {
+        int64_t i_time = var_InheritInteger( p_input, TIME_TO_TRANSLATE );
+        int64_t i_num;
+        char *tip = NULL;
+
+        if ( p_sys->is_origin )
+        {
+            var_Get( p_input, "audio-es", &p_sys->origin_audiotrack );
+            var_Get( p_input, "spu-es", &p_sys->origin_subtrack );
+            p_sys->is_origin = false;
+        }
+
+        switch ( newval.i_int )
+        {
         /* translate the past few seconds */
         case KEY_SLASH:
-        {
-            if( p_input && var_GetBool( p_input, "can-seek" ) )
-            {
-                /* change a language */
-                int64_t i_num = var_InheritInteger( p_input, TRANSLATED_AUDIOTRACK_NUM );
-                ChangeAudioTrack( p_input, i_num );
-                /* move backward */
-                int64_t i_time = var_InheritInteger( p_input, TIME_TO_TRANSLATE );
-                msg_Dbg( libvlc, "move backward %" PRId64 " sec", i_time );
-            
-                p_sys->i_end_time = var_GetInteger( p_input, "time" );
-                p_sys->i_start_time = p_sys->i_end_time - i_time * CLOCK_FREQ;
-                var_SetInteger( p_input, "time", p_sys->i_start_time );
-                DisplayMessage(p_vout, p_sys->i_end_time - p_sys->i_start_time, "TIP: translate");
-                vlc_timer_schedule( p_sys->timer, false, i_time * CLOCK_FREQ, 0 );
-            }
+            i_num = var_InheritInteger( p_input, TRANSLATE_SUBTRACK_NUM );
+            ChangeSubTrack( p_input, i_num );
+
+            i_num = var_InheritInteger( p_input, TRANSLATE_AUDIOTRACK_NUM );
+            ChangeAudioTrack( p_input, i_num );
+
+            p_sys->i_end_time = var_GetInteger( p_input, "time" );
+            p_sys->i_start_time = p_sys->i_end_time - i_time * CLOCK_FREQ;
+            tip = "TIP: translate";
+            break;
+        /* repeat the last translated period */
+        case KEY_SHIFT_QUESTION:
+            i_num = var_InheritInteger( p_input, REPEAT_SUBTRACK_NUM );
+            ChangeSubTrack( p_input, i_num );
+
+            var_Set( p_input, "audio-es", p_sys->origin_audiotrack );
+            tip = "TIP: repeat";
             break;
         }
-        /* repeat the last translated period */    
-        case KEY_SHIFT_QUESTION:
-        {
-            if ( p_sys->i_start_time >= 0 && p_input && var_GetBool(p_input, "can-seek") )
-            {
-                int64_t i_num = var_InheritInteger( p_input, MAIN_AUDIOTRACK_NUM );
-                ChangeAudioTrack( p_input, i_num );
-                var_SetInteger( p_input, "time", p_sys->i_start_time );
-                DisplayMessage( p_vout, p_sys->i_end_time - p_sys->i_start_time, "TIP: repeat" );
-                break;
-            }
-        }
+
+        msg_Dbg( libvlc, "move backward %" PRId64 " sec", i_time );
+        var_SetInteger( p_input, "time", p_sys->i_start_time );
+        DisplayMessage( p_vout, p_sys->i_end_time - p_sys->i_start_time, tip );
+        vlc_timer_schedule( p_sys->timer, false, i_time * CLOCK_FREQ, 0 );
     }
     
-    if( p_input != NULL )
+    if ( p_input != NULL )
         vlc_object_release( p_input );
-    if( p_vout != NULL )
+    if ( p_vout != NULL )
         vlc_object_release( p_vout );
     
     return VLC_SUCCESS;
@@ -223,12 +252,14 @@ static int KeyboardEvent( vlc_object_t *libvlc, char const *psz_var,
 static int PlaylistEvent( vlc_object_t *p_this, char const *psz_var,
                           vlc_value_t oldval, vlc_value_t newval, void *p_data )
 {
-    VLC_UNUSED( p_this ); 
-    VLC_UNUSED( psz_var ); 
+    VLC_UNUSED( p_this );
+    VLC_UNUSED( psz_var );
     VLC_UNUSED( oldval );
     intf_thread_t *p_intf = p_data;
 
     ChangeInput( p_intf, newval.p_address );
+    p_intf->p_sys->i_start_time = 0;
+    p_intf->p_sys->is_origin = true;
 
     return VLC_SUCCESS;
 }
@@ -244,7 +275,7 @@ static int InputEvent( vlc_object_t *p_this, char const *psz_var,
     VLC_UNUSED( psz_var );
     VLC_UNUSED( oldval );
 
-    if( newval.i_int == INPUT_EVENT_VOUT )
+    if ( newval.i_int == INPUT_EVENT_VOUT )
         ChangeVout( p_intf, input_GetVout( p_input ) );
 
     return VLC_SUCCESS;
@@ -253,27 +284,34 @@ static int InputEvent( vlc_object_t *p_this, char const *psz_var,
 /*****************************************************************************
  * TimerEvent: callback for processing timer events
  *****************************************************************************/
-static void TimerEvent(void *p_data) {
-    intf_thread_t *p_intf = (intf_thread_t *) p_data;
+static void TimerEvent( void *p_data )
+{
+    intf_thread_t *p_intf = (intf_thread_t *)p_data;
     intf_sys_t *p_sys = p_intf->p_sys;
 
     msg_Dbg( p_intf, "timer event" );
-    int64_t i_num = var_InheritInteger( p_sys->p_input, MAIN_AUDIOTRACK_NUM );
-    ChangeAudioTrack( p_sys->p_input, i_num );
+    var_Set( p_sys->p_input, "spu-es", p_sys->origin_subtrack );
+    var_Set( p_sys->p_input, "audio-es", p_sys->origin_audiotrack );
+    p_sys->is_origin = true;
 }
 
-static void ChangeAudioTrack( input_thread_t* p_input, int64_t i_num ) {
-    msg_Dbg( p_input, "change an audio track to #%" PRId64, i_num );
-    vlc_value_t audio_tracks;
-    var_Change( p_input, "audio-es", VLC_VAR_GETCHOICES, &audio_tracks, NULL );
+static void SetVarByValueIndex( input_thread_t* p_input, char const *psz_name,
+                                int64_t i_index )
+{
+    msg_Dbg( p_input, "change %s to #%" PRId64, psz_name, i_index );
+    if ( i_index < 0 )
+        return;
 
-    int i_count = audio_tracks.p_list->i_count;
-    if (i_num > 0 && i_num < i_count) {
-        var_Set( p_input, "audio-es", audio_tracks.p_list->p_values[i_num] );
-    } else
-        msg_Err( p_input, "wrong audio track number, the value should be an integer from 1 to %d", i_count - 1 );
+    vlc_value_t list;
+    var_Change( p_input, psz_name, VLC_VAR_GETCHOICES, &list, NULL );
+    int i_count = list.p_list->i_count;
 
-    var_FreeList( &audio_tracks, NULL );
+    if ( i_index < i_count )
+        var_Set( p_input, psz_name, list.p_list->p_values[i_index] );
+    else
+        msg_Err( p_input, "%s value should be < %d", psz_name, i_count );
+
+    var_FreeList( &list, NULL );
 }
 
 static void ChangeInput( intf_thread_t *p_intf, input_thread_t *p_input )
@@ -282,7 +320,7 @@ static void ChangeInput( intf_thread_t *p_intf, input_thread_t *p_input )
 
     input_thread_t *p_old_input = p_sys->p_input;
     vout_thread_t *p_old_vout = NULL;
-    if( p_old_input != NULL )
+    if ( p_old_input != NULL )
     {
         /* First, remove callbacks from previous input. 
          * It's safe to access it unlocked, since it's written from this thread */
@@ -297,15 +335,15 @@ static void ChangeInput( intf_thread_t *p_intf, input_thread_t *p_input )
     vlc_mutex_unlock( &p_sys->lock );
 
     /* Release old input and vout objects unlocked */
-    if( p_old_input != NULL )
+    if ( p_old_input != NULL )
     {
-        if( p_old_vout != NULL )
+        if ( p_old_vout != NULL )
             vlc_object_release( p_old_vout );
         vlc_object_release( p_old_input );
     }
 
     /* Register input events */
-    if( p_input != NULL )
+    if ( p_input != NULL )
         var_AddCallback( p_input, "intf-event", InputEvent, p_intf );
 }
 
@@ -318,7 +356,7 @@ static void ChangeVout( intf_thread_t *p_intf, vout_thread_t *p_vout )
     p_sys->p_vout = p_vout;
     vlc_mutex_unlock( &p_sys->lock );
 
-    if( p_old_vout != NULL )
+    if ( p_old_vout != NULL )
     {
         vlc_object_release( p_old_vout );
     }
